bc07b8766a74fc8710854b3526459ecd1ca2f7b2
Park Chan <zxc88kr@naver.com>
2024-06-06 18:25:53 +0900

    feat: Category 엔티티 생성

--- src/main/java/com/jnu/gonggam/category/domain/Category.java
+++ src/main/java/com/jnu/gonggam/category/domain/Category.java
@@ -1,14 +1,24 @@
 package com.jnu.gonggam.category.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 @Table(name = "category_tb")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_pk")
+    private int id;
+
+    @NotNull
+    @Column(length = 45)
+    private String name;
 }

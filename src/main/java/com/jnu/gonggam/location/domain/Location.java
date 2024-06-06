acca3d9ca407cfe49da22ce469144c35354f6028
Park Chan <zxc88kr@naver.com>
2024-06-06 18:21:34 +0900

    feat: Location 엔티티 생성

--- src/main/java/com/jnu/gonggam/location/domain/Location.java
+++ src/main/java/com/jnu/gonggam/location/domain/Location.java
@@ -1,14 +1,30 @@
 package com.jnu.gonggam.location.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 @Table(name = "location_tb")
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_pk")
+    private Long id;
+
+    @NotNull
+    @Column(length = 200)
+    private String name;
+
+    private BigDecimal latitude;
+
+    private BigDecimal longitude;
 }

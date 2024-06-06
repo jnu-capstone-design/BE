b71bbbc20c39e4af085cdb2fc43cd17312fe6a91
Park Chan <zxc88kr@naver.com>
2024-06-06 13:10:05 +0900

    build: 개발환경 기초 세팅

---
+++ src/main/java/com/example/gonggam/GonggamApplication.java
@@ -0,0 +1,13 @@
+package com.example.gonggam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GonggamApplication.class, args);
+    }
+
+}

c7694bbdb29c533f24490519945417471dce0a57
Park Chan <zxc88kr@naver.com>
2024-06-06 17:42:36 +0900

    feat: Review 엔티티 생성

--- src/main/java/com/jnu/gonggam/review/domain/Review.java
+++ src/main/java/com/jnu/gonggam/review/domain/Review.java
@@ -1,12 +1,18 @@
 package com.jnu.gonggam.review.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
+import com.jnu.gonggam.product.domain.Product;
+import com.jnu.gonggam.purchase.domain.Purchase;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -14,4 +20,47 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Entity
 @Table(name = "review_tb")
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_pk")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "purchase_pk")
+    private Purchase purchase;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_pk")
+    private Product product;
+
+    @NotNull
+    private int starAverage;
+
+    @NotNull
+    @Column(length = 600)
+    private String content;
+
+    @Column(length = 200)
+    private String reviewImagePath;
+
+    @NotNull
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @NotNull
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+
+    private LocalDateTime deleteAt;
+
+    @Builder
+    public Review(Long id, Purchase purchase, Product product,
+                  int starAverage, String content, String reviewImagePath) {
+        this.id = id;
+        this.purchase = purchase;
+        this.product = product;
+        this.starAverage = starAverage;
+        this.content = content;
+        this.reviewImagePath = reviewImagePath;
+    }
 }

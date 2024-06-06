9ee20232608eb3d79f2181af5d17ed6213eeeade
Park Chan <zxc88kr@naver.com>
2024-06-06 16:57:28 +0900

    feat: User 엔티티 생성

--- src/main/java/com/jnu/gonggam/user/domain/User.java
+++ src/main/java/com/jnu/gonggam/user/domain/User.java
@@ -1,12 +1,18 @@
 package com.jnu.gonggam.user.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
+import com.jnu.gonggam.location.domain.Location;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -14,4 +20,62 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Entity
 @Table(name = "user_tb")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_pk")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_pk")
+    private Location location;
+
+    @NotNull
+    @Column(length = 20)
+    private String identifier;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @NotNull
+    @Column(length = 45)
+    private String name;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @NotNull
+    @Column(length = 20)
+    private String telephone;
+
+    @NotNull
+    private LocalDate birth;
+
+    @NotNull
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @NotNull
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+
+    private LocalDateTime deleteAt;
+
+    @Builder
+    public User(Long id, Location location, String identifier, String password,
+                Role role, String name, Gender gender, String telephone, LocalDate birth) {
+        this.id = id;
+        this.location = location;
+        this.identifier = identifier;
+        this.password = password;
+        this.role = role;
+        this.name = name;
+        this.gender = gender;
+        this.telephone = telephone;
+        this.birth = birth;
+    }
 }

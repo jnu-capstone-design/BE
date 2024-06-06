0dee82243f88f2f4880c6a5459c3acd8dd240f36
Park Chan <zxc88kr@naver.com>
2024-06-06 17:50:40 +0900

    feat: ProductImage 엔티티 생성

--- src/main/java/com/jnu/gonggam/product/domain/ProductImage.java
+++ src/main/java/com/jnu/gonggam/product/domain/ProductImage.java
@@ -1,14 +1,26 @@
 package com.jnu.gonggam.product.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 @Table(name = "product_image_tb")
 public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_image_pk")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "product_pk")
+    private Product product;
+
+    @Column(length = 200)
+    private String productImagePath;
 }
